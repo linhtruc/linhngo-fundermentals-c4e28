241e6c0159126f11ee3a42a0e2c5c930fdc14809
linhtruc <truclinh.ngo08@gmail.com>
2019-02-22 22:10:11 +0700

    vdcskjfh

---
+++ Section1/exam1.py
@@ -0,0 +1,4 @@
+n = input ("Enter your birth year: ")
+age = 2019 - int(n)
+print("Your age: ", age)
+
---
+++ Section1/intro.py
@@ -0,0 +1,8 @@
+# n = "Linh" 
+
+# print ("Hello")
+n = "Linh"
+n = "Binh"
+n = input("Enter your word, please!")
+print (n) 
+
