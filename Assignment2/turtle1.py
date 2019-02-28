b62a7c555b4e627a4f00a091ba6f48c4da4c2882
linhtruc <truclinh.ngo08@gmail.com>
2019-02-28 23:16:29 +0700

    Create turtle1.py

---
+++ Assignment2/turtle1.py
@@ -0,0 +1,13 @@
+#Program draw1
+from turtle import *
+for i in range (4):
+    left (30)
+    forward (200)
+    right (60)
+    forward (200)
+    right (120)
+    forward (200)
+    right (60)
+    forward (200)
+    right (60)
+mainloop()
