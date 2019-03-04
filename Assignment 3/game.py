b6c81baffefd7d3ea389a0d0c86e9badb8526a85
linhtruc <truclinh.ngo08@gmail.com>
2019-03-05 01:32:30 +0700

    Create game.py

---
+++ Assignment 3/game.py
@@ -0,0 +1,26 @@
+# Jumble game about fruit
+import random
+print("Welcome to fruit jumble game!!!")
+n = input("Press any key to enter")
+fruit = ["pineapple", "pomegranate", "strawberry", "star fruit", "mango", "lemon", "avocado"]
+print("Let's start!")
+print("You have 5 times")
+word = random.choice(fruit)
+l = list (word)
+random.shuffle(l)
+print ()
+print (*l, sep= '-')
+print ()
+loop = True
+count = 0
+while loop:
+    ans = input("Enter your answer: ")
+    count += 1
+    if ans == word:
+        print ("Excellent!!!")
+        loop = False
+    elif count ==5:
+        print("You lose")
+        break
+    else:
+        print ("Try again")
