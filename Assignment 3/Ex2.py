217471526a8806bf188948046f59ca7222c64222
linhtruc <truclinh.ngo08@gmail.com>
2019-03-05 00:34:03 +0700

    Update Ex2.py

--- Assignment 3/Ex2.py
+++ Assignment 3/Ex2.py
@@ -3,7 +3,7 @@ sheep = [12, 34, 68, 478, 723, 98, 34, 87, 406, 279, 29, 1, 4, 8]
 #1
 print ("Hello, I'm Moon Jellyfish!")
 print ("There are my sheep sizes: ")
-print (*sheep, sep=", ")
+print (sheep)
 
 #2
 print ("My biggest sheep size: ",max(sheep))
@@ -12,52 +12,52 @@ print ("My biggest sheep size: ",max(sheep))
 position = sheep.index(max(sheep))
 print ("Bigest is: ", sheep[position])
 print ("Biggest size replace by 8, new sheep sizes: ")
-sheep [position] = 8
-print (*sheep, sep=", ")
+sheep[position] = 8
+print (sheep)
 
 #4
-print()
-print ("MONTH 1:")
-for item in sheep:
-    item = item + 50
-    print (item, end=', ')
 
-print ()
-print ("Biggest is: ", sheep[position])
-print ("Biggest size replace by 8, new sheep sizes: ")
-position = sheep.index(max(sheep))
-sheep[position] = 8
-print (*sheep, sep=", ")
-
-#6
-print ()
-print ("MONTH 2:")
-for item in sheep:
-    item = item + 100
-    print (item, end=', ')
-
-print ()
-print ("Biggest is: ", sheep[position])
-print ("Biggest size replace by 8, new sheep sizes: ")
-position = sheep.index(max(sheep))
-sheep[position] = 8
-print (*sheep, sep=", ")
-
-print ()
-print ("MONTH 3:")
-for item in sheep:
-    item = item + 150
-    print (item, end=', ')
-    
-print ()
-print ("Biggest is: ", sheep[position])
-print ("Biggest size replace by 8, new sheep sizes: ")
-position = sheep.index(max(sheep))
-sheep[position] = 8
-print (*sheep, sep=", ")
+for i in range(int(len(sheep))):
+    sheep[i] = sheep[i] + 50
+print (sheep)
 
 #5
+print ()
+print ("MONTH 1:")
+for i in range(int(len(sheep))):
+    sheep[i] = sheep[i] + 50
+print (sheep)
+print ()
+position = sheep.index(max(sheep))
+print ("Biggest is: ", sheep[position])
+print ("Biggest size replace by 8, new sheep sizes: ")
+sheep[position] = 8
+print (sheep)
+
+print()
+print ("MONTH 2:")
+for i in range(int(len(sheep))):
+    sheep[i] = sheep[i] + 50
+print (sheep)
+print ()
+position = sheep.index(max(sheep))
+print ("Biggest is: ", sheep[position])
+print ("Biggest size replace by 8, new sheep sizes: ")
+sheep[position] = 8
+print (sheep)
+
+print()
+print ("MONTH 2:")
+for i in range(int(len(sheep))):
+    sheep[i] = sheep[i] + 50
+print (sheep)
+print ()
+position = sheep.index(max(sheep))
+print ("Biggest is: ", sheep[position])
+print ("Biggest size replace by 8, new sheep sizes: ")
+sheep[position] = 8
+print (sheep)
+
+#6
 total = sum(sheep)
-print ("I would have:", total, "* 2$ = ", total * 2, "$")
-
-
+print ("I would get", total, "* 2$ = ", total * 2, "$")
