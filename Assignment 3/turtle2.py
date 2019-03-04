0762027c40daf178fb109a5e90dd2447b74ab494
linhtruc <truclinh.ngo08@gmail.com>
2019-03-04 23:43:46 +0700

    Create turtle2.py

---
+++ Assignment 3/turtle2.py
@@ -0,0 +1,48 @@
+#Program draw2
+from turtle import *
+
+color ('grey')
+begin_fill()
+for i in range (2):
+    forward (250)
+    left (90)
+    forward (100)
+    left(90)
+end_fill()
+
+color ('yellow')
+begin_fill()
+for i in range (2):
+    forward (200)
+    left (90)
+    forward (100)
+    left(90)
+end_fill()
+
+color ('brown')
+begin_fill()
+for i in range (2):
+    forward (150)
+    left (90)
+    forward (100)
+    left(90)
+end_fill()
+
+color ('blue')
+begin_fill()
+for i in range (2):
+    forward (100)
+    left (90)
+    forward (100)
+    left(90)
+end_fill()
+
+color ('red')
+begin_fill()
+for i in range (2):
+    forward (50)
+    left (90)
+    forward (100)
+    left(90)
+end_fill()
+mainloop()
