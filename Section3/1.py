1c3ed9ea961e1ca8200b7ae1ec712eb329449d56
linhtruc <truclinh.ngo08@gmail.com>
2019-03-04 23:43:55 +0700

    Create 1.py

---
+++ Section3/1.py
@@ -0,0 +1,33 @@
+# While 
+# while  True:
+#     print("Hello")
+
+# Ctrl + C để dừng vòng lặp
+# count = 0
+# while count < 3:
+#     print ("Hello")
+#     # count = count + 1
+#     count += 1
+
+
+# count = 0
+# loop = True
+# while loop:
+#     print ("Hello")
+#     count += 1
+#     if count == 5:
+#         loop = False
+#         # break
+#         print ("bye bye")
+# # in dòng cuối sau khi dừng
+
+count = 0
+loop = True
+while loop:
+    print ("Hello")
+    count += 1
+    if count == 5:
+        # loop = False
+        break
+        print ("bye bye")
+# bỏ qua các lệnh sau đó, không in dòng cuối
