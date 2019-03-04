5eee6e560eac30745455fb9e2e62345801e248f3
linhtruc <truclinh.ngo08@gmail.com>
2019-03-04 23:43:53 +0700

    Create 2.py

---
+++ Section3/2.py
@@ -0,0 +1,76 @@
+#List / Array
+menu = ["Com", "Bun", "Pho", "Rau", "Banh cuon"]
+
+# menu = ["Com", "Bun", "Pho", "Rau", "Banh cuon"]
+# menu.append("Chao")
+
+# print (*menu, sep= " - ")
+
+
+# n = input("Enter new one: ")
+# menu = ["Com", "Bun", "Pho", "Rau", "Banh cuon"]
+# menu.append (n)
+# print (*menu, sep= " - ")
+
+# index in list
+
+# for i in range (5):
+#     print (i+1, end=" ")
+#     menu = ["Com", "Bun", "Pho", "Rau", "Banh cuon"]
+#     print (menu[i])
+
+# #loop with item
+# for item in menu:
+#     print(item)
+
+# #loop with index
+# for index in range(len(menu)):
+#     print(index)
+
+# #loop with item, index
+# for index, item in enumerate(menu):
+#     print(index+1, item)
+
+# n = input("Enter new one: ")
+# menu = ["Com", "Bun", "Pho", "Rau", "Banh cuon"]
+# menu.append (n)
+# print (*menu, sep= " - ")
+
+# for index, item in enumerate(menu):
+#     print(index+1, item)
+
+# Update
+
+# menu[0] = "Chao"
+# print(*menu, sep=" - ")
+
+# for index, item in enumerate(menu):
+#     print(index+1, item)
+
+# n = int(input("Enter number update: "))
+# m = input ("Enter new thing: ")
+# menu [n-1] = m
+
+# for index, item in enumerate(menu):
+#     print(index+1, item)
+
+# Delete
+
+# menu.pop(1)
+# print(*menu, sep=" - ")
+
+# del menu[1]
+# print (*menu, sep=" - ")
+
+# for index, item in enumerate(menu):
+#     print(index+1, item)
+
+# n = int(input("Enter position update: "))
+
+# del menu[n-1]
+# for index, item in enumerate(menu):
+#     print(index+1, item)
+
+
+myList = [ 4,'a', 'b', 'c', 1 'd', 3]
+print(myList)
