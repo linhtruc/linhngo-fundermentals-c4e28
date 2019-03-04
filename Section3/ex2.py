5636041009cab24cb8d07df03d0f2db70405a9bf
linhtruc <truclinh.ngo08@gmail.com>
2019-03-04 23:44:02 +0700

    Create ex2.py

---
+++ Section3/ex2.py
@@ -0,0 +1,20 @@
+n = int(input("Enter a number: "))
+count = 0
+# loop = True
+# while loop:
+#     if n > 0:
+#         n = n // 10
+#         count += 1
+#     else:
+#         loop = False
+#         print ("Number of digits: ", count)
+
+# while n > 0:
+#     n = n // 10
+#     count += 1
+#     print(n)
+# print (count)
+
+count = len (n)
+print (count)
+
