a52ec6c68300fcd8b78068d4968c1b0befbfa8b3
linhtruc <truclinh.ngo08@gmail.com>
2019-03-04 23:43:40 +0700

    Create turtle1.py

---
+++ Assignment 3/turtle1.py
@@ -0,0 +1,28 @@
+#Program draw1
+from turtle import *
+color ('red')
+for i in range (3):
+    forward (100)
+    left(120)
+
+color ('blue')
+for i in range (4):
+    forward (100)
+    left (90)
+
+color ('brown')
+for i in range (5):
+    forward (100)
+    left (72)
+
+color ('yellow')
+for i in range (6):
+    forward (100)
+    left (60)
+
+color ('gray')
+for i in range (7):
+    forward (100)
+    left (180-900/7)
+
+mainloop()
