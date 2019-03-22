b42da35dda9c7f1b2c2cad4585cab68a3cc47cfe
linhtruc <truclinh.ngo08@gmail.com>
2019-03-22 12:13:04 +0700

    Create ex1.py

---
+++ Web/Assignment7/ex1.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ("Hello!")
+
+@app.route('/about_me')
+def about_me():
+    return ('''
+    你是谁啊？
+    ''')
+
+@app.route('/school')
+def school():
+    return redirect('http://techkids.vn ')
+
+if __name__ == '__main__':
+  app.run(debug=True)
